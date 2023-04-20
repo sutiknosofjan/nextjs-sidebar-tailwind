a4f231596352a566ef870b8e91b26013c096a6fb
Sutikno Sofjan <59943183+sutiknosofjan@users.noreply.github.com>
2023-04-20 14:54:49 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -26,36 +26,30 @@ export default function Home() {
   ];
 
   return (
-    <div className="flex">
-      <div className={` bg-gray-800 text-white h-screen p-5 pt-8 ${open ? "w-65" : "w-20"} duration-300 relative`}>
-        <BsArrowLeftShort className={`bg-white text-black
-        text-3xl rounded-full absolute -right-4 border
-        cursor-pointer ${!open && "rotate-180"}`} onClick={() => setOpen(!open && "rotate-180")} />
+    <div id="main" className="flex">
+      <div id="box-sidebar" className={` bg-gray-800 text-white h-screen p-5 pt-3 ${open ? "w-65" : "w-20"} duration-300 relative`}>
+        <BsArrowLeftShort id="toggle-sidebar-colapse" className={`bg-gray-500 text-gray-300 text-3xl rounded-full absolute -right-3 cursor-pointer ${!open && "rotate-180"}`} onClick={() => setOpen(!open && "rotate-180")} />
 
-        <div className="inline-flex ">
-          <SiNextdotjs className={` bg-gray-500 text-white 
-          text-4xl rounded cursor-pointer float-left p-1 mr-3 duration-500
-          ${open && "rotate-[360deg]"}`} />
+        <div id="box-brand" className="inline-flex ">
+          <SiNextdotjs className={` bg-gray-500 text-white text-4xl rounded cursor-pointer float-left p-1 mr-3 duration-300 ${open && "rotate-[360deg]"}`} />
           <h1 className={`text-white origin-left font-medium text-2xl duration-300 ${!open && "scale-0"}`}>
             NEXTo
           </h1>
         </div>
 
-        <div className={`flex item-center bg-gray-600 rounded-md mt-6 py-2 ${!open ? "px-3" : "px-5"}`}>
-          <BsSearch className={`text-gray-900 text-md block float-left cursor-pointer duration-200 ${open && "mt-1 mr-2"}`} />
-          <input type="text" value={_search} onChange={(e) => setSearch(e.target.value)} placeholder="Search"
-            className={`text-base text-gray-300 bg-transparent w-full outline-none duration-200 ${!open && "hidden"}`} />
+        <div id="box-search" className={`flex item-center bg-gray-600 rounded-md mt-6 py-2 ${!open ? "px-3" : "px-5"}`}>
+          <BsSearch className={`text-gray-900 text-md block float-left cursor-pointer duration-300 ${open && "mt-1 mr-2"}`} />
+          <input type="text" value={_search} onChange={(e) => setSearch(e.target.value)} placeholder="Search" className={`text-base text-gray-300 bg-transparent w-full outline-none duration-200 ${!open && "hidden"}`} />
         </div>
 
-        <ul className="pt-2">
+        <ul id="sidebar-menu" className="pt-2">
           {Menus.map((menu, index) => (
             <>
-              <li key={index} className={`text-gray-400 text-sm flex items-center gap-x-4 
-                cursor-pointer p-2 hover:bg-gray-700 hover:text-white duration-100 rounded-md ${menu.spacing ? "mt-9" : "mt-2"}`} onClick={() => setSubmenuOpen(!submenuOpen)}>
+              <li key={index} className={`text-gray-400 text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-gray-700 hover:text-white duration-100 rounded-md ${menu.spacing ? "mt-8" : "mt-1"}`} onClick={() => setSubmenuOpen(!submenuOpen)}>
                 <span className="text-2xl block float-left">
                   {menu.icon ? menu.icon : <BsDashSquareFill />}
                 </span>
-                <span className={`text-base font-medium flex-1 duration-100 ${!open && "hidden"}`}>
+                <span className={`text-base flex-1 duration-100 ${!open && "hidden"}`}>
                   {menu.title}
                 </span>
                 {menu.submenu && open && (
@@ -63,7 +57,7 @@ export default function Home() {
                 )}
               </li>
               {menu.submenu && submenuOpen && open && (
-                <ul>
+                <ul id="sidebar-submenu">
                   {menu.submenuItems.map((submenuItem, index) => (
                     <li key={index} className="text-gray-400 text-sm flex items-center gap-x-4 
                       cursor-pointer p-2 px-5 hover:bg-gray-700 hover:text-white rounded-md">
@@ -77,16 +71,27 @@ export default function Home() {
         </ul>
       </div>
 
-      <div className="p-7 w-full">
-        <h1 className="text-2xl font-semibold">Home Page</h1>
-
-        <div className="shadow-md w-full mt-5 p-3">
-          lorem lorem lorem lorem lorem lorem lorem lorem<br />
-          lorem lorem lorem lorem lorem lorem lorem lorem<br />
-          lorem lorem lorem lorem lorem lorem lorem lorem<br />
-          lorem lorem lorem lorem lorem lorem lorem lorem<br />
-          lorem lorem lorem lorem lorem lorem lorem lorem<br />
+      <div id="content" className="w-full h-screen">
+        <div id="navbar" className="bg-gray-500 text-white font-semibold p-4 border-bottom">
+          menuNav1
         </div>
+
+        <div id="content-body" className="p-3 w-full mb-10">
+          <h1 className="text-2xl font-semibold">title: Home Page</h1>
+
+          <div className="shadow-md w-full mt-5 p-3 border rounded-xl">
+            content : lorem lorem lorem lorem lorem lorem lorem<br />
+            lorem lorem lorem lorem lorem lorem lorem lorem<br />
+            lorem lorem lorem lorem lorem lorem lorem lorem<br />
+            lorem lorem lorem lorem lorem lorem lorem lorem<br />
+            lorem lorem lorem lorem lorem lorem lorem lorem<br />
+          </div>
+        </div>
+
+        <div id="footer" className="bg-gray-200 text-gray-500 text-xs p-3 text-right">
+          Copyright nexto.id &copy;2023
+        </div>
+
       </div>
     </div>
   )
